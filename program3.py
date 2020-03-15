c06cacc5d438d774e7b803a6cdc6bb7f0256848b
AjayAnandC <54232958+AjayAnandC@users.noreply.github.com>
2020-03-15 23:13:14 +0530

    Create program3.py
    3. Given an object/dictionary, containing names as keys and amount_paid by each of them as
    values, write a function that takes such an object as input and calculates the total sum paid by them together.
    
    Below is an example:
    
    Input: {"Rick": 85, "Amit": 42, "George": 53, "Tanya": 60, "Linda": 35}
    Output: 275

---
+++ program3.py
@@ -0,0 +1,11 @@
+def lst_add(a):
+    ans=0
+    n=len(a)
+    for i in range(n):
+        ans=ans+a[i]
+    return ans    
+import math
+a={"Rick": 85, "Amit": 42, "George": 53, "Tanya": 60, "Linda": 35}
+list_values = [ v for v in a.values() ]
+print("Sum of values:",lst_add(list_values))
+
